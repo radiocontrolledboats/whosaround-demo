6d1ab54a3f232daca7367d98c4cc021ecfab0888
jokersus <lolivampireslave@gmail.com>
2019-05-19 02:01:06 +0400

    Boilerplate express server

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+	// Homepage here
+	res.send('hello world');
+});
+
+app.get('/register/:name/:location', (req, res) => {
+
+});
+
+app.listen(process.env.PORT || 3000);
